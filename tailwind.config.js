7b37b7657882dc437f9aa53b523c648f99111af7
Ridho Maulana <edoin21@gmail.com>
2021-04-27 14:31:08 +0700

    Add Inter as default font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans:
+        'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
     extend: {},
   },
   variants: {
